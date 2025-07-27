5cc1211576127096922f07844160aa18a0f5357f
Horry <nikitaskvortsov22@gmai.com>
2025-07-27 20:49:57 +0300

    Initial commit: Rust project setup

---
+++ main.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    let name = String::from("Horry");
+    let age: u32 = 21;
+
+    println!("My name is {}", name);
+    println!("I'm {} years old", age);
+    println!("Nice to meet you.");
+
+}
