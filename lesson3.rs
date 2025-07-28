b936772a18aa5d18237cfa7558372ff85cedf8b3
Horry <nikitaskvortsov22@gmai.com>
2025-07-28 03:28:06 +0300

    Lesson 3: Basic types implementation

---
+++ lesson3.rs
@@ -0,0 +1,19 @@
+fn main(){
+    let temperature: i16 = -10;
+    println!("Temperature: {}C", temperature);
+    
+    let temperature: i16 = temperature + 20;
+    println!("Warmed temperature: {}C", temperature);
+
+    let mut pi_approx: f64 = 3.14;
+    pi_approx = pi_approx + 0.00159;
+    println!("Approximate Pi {}", pi_approx);
+
+    let is_positive: bool = true; 
+    let unit = '°';
+
+    println!("Is warmed temp positive? {}, Unit {}", is_positive, unit);
+
+    let user = String::from("Horry");
+    println!("Calculated by: {}", user);
+}
