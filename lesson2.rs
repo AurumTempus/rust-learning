22ff77a95bc1811fa7d4b657f6b7323e2d2c5337
Horry <nikitaskvortsov22@gmai.com>
2025-07-28 03:07:17 +0300

    Fixed Lesson 2: Corrected new_score to score, renamed name_user to name

--- lesson2.rs
+++ lesson2.rs
@@ -1,17 +1,16 @@
 fn main() {
-    
     let score: i32 = 0;
     println!("Initial score: {}", score);
 
     let score: i32 = score + 10;
-    println!("Updated score: {}", new_score);
+    println!("Updated score: {}", score);
 
     let mut bonus: i32 = 5;
     bonus = bonus + 3;
 
-    println!("Final score with bonus: {}", new_score + bonus);
+    println!("Final score with bonus: {}", score + bonus);
 
-    let name_user = String::from("Horry");
-    println!("Player: {}", name_user);
+    let name = String::from("Horry");
+    println!("Player: {}", name);
 
 }
